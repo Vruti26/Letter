7d0e77db95c196b282621e6751af6cb77fe7958d
Vruti Rupapara <rupaparavruti@gmail.com>
2025-12-07 09:06:32 +0000

    OMG

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-});
+    url: 'https://beloved-dane-19347.upstash.io',
+    token: 'AUuTAAIncDIzNTQ1ZDI0Y2UxMWI0NDYyYjMzZDVjNTc4MTk1ZmEyYXAyMTkzNDc',
+  });
